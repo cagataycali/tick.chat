4e5fa87d1c20600597dfe303fd0ca564757f0ff6
cagataycali <cagataycali@icloud.com>
2023-02-10 00:47:27 +0100

    test

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import Framework7 from 'framework7'
 import Framework7Vue from 'framework7-vue'
 import VueSocketio from 'vue-socket.io'
 
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-import animations from './css/animation.css'
-import AppStyles from './css/app.css'
+// import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
+// import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+// import animations from './css/animation.css'
+// import AppStyles from './css/app.css'
 
 import App from './app'
 import Map from './components/Map.vue'
@@ -22,14 +22,13 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store(storeOptions)
 
-
 Vue.use(Framework7Vue)
-// if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   Vue.use(VueSocketio, 'http://localhost:3000', store)
-// } else {
-//   Vue.use(VueSocketio, 'https://service.tick.chat', store)
-// }
-
+} else {
+  Vue.use(VueSocketio, 'https://service.yardimiste.xyz', store)
+}
+// 
 Vue.use(VueGoogleMaps, {
   installComponents: true,
   load: {
